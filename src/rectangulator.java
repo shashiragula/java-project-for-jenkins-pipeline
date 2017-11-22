83952b01867b1d17954104471400017d2b6da705
shashi <shashiragula@gmail.com>
2017-11-22 06:42:14 +0000

    syntax error

--- src/rectangulator.java
+++ src/rectangulator.java
@@ -1,6 +1,6 @@
 public class Rentangulator {
 	public static void main(String[] args) {
-	  int length = Integer.[parseInt(args[0]);
+	  int length = Integer.parseInt(args[0]);
 	  int width = Integer.parseInt(args[1]);
 
 	  Rectangle myRectangle = new Rectangle(length, width);
